ea95c216219ff2906df370eb8c4b44a5e4696a90
SuperJokerayo <1072843554@qq.com>
2024-05-11 01:37:00 +0800

    add notion_text_display

--- notion_text_display/content.js
+++ notion_text_display/content.js
@@ -8,25 +8,19 @@ function addStyleToElements() {
   }
 
 window.addEventListener('load', (event) => {
-  console.log("hhhhhhhhhhhhhhhhh")
   if (document.documentElement.classList.contains('notion-html')) {
-      console.log("hhhhhhhhhhhhhhhhh===============")
     addStyleToElements();
   }
-  console.log("===============hhhhhhhhhhhhhhhhh")
-  // 监听页面内容的变化
+
   var observer = new MutationObserver(function(mutationsList, observer) {
     mutationsList.forEach(function(mutation) {
       if (mutation.type === 'childList' || (mutation.type === 'attributes' && mutation.attributeName === 'class')) {
-        // 检查页面的根元素是否包含 'notion' 类名
         if (document.documentElement.classList.contains('notion-html')) {
-          // 添加样式
           addStyleToElements();
         }
       }
     });
   });
 
-  // 监听根元素的变化
   observer.observe(document.documentElement, { attributes: true, childList: true, subtree: true });
 });
