0aa7fc3ae8ab7bb6361b3d1ada322bf93ca0c43a
SuperJokerayo <1072843554@qq.com>
2024-05-11 11:59:23 +0800

    adjust retraction

--- notion_text_display/content.js
+++ notion_text_display/content.js
@@ -1,26 +1,26 @@
 function addStyleToElements() {
-    var elementsWithClassA = document.getElementsByClassName('notranslate');
+    var elements = document.getElementsByClassName('notranslate');
     
-    for (var i = 0; i < elementsWithClassA.length; i++) {
-        elementsWithClassA[i].style.textAlign = 'justify';
-        elementsWithClassA[i].style.display = 'block';
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.textAlign = 'justify';
+        elements[i].style.display = 'block';
     }
   }
 
 window.addEventListener('load', (event) => {
-  if (document.documentElement.classList.contains('notion-html')) {
-    addStyleToElements();
-  }
+    if (document.documentElement.classList.contains('notion-html')) {
+        addStyleToElements();
+    }
 
-  var observer = new MutationObserver(function(mutationsList, observer) {
-    mutationsList.forEach(function(mutation) {
-      if (mutation.type === 'childList' || (mutation.type === 'attributes' && mutation.attributeName === 'class')) {
-        if (document.documentElement.classList.contains('notion-html')) {
-          addStyleToElements();
-        }
-      }
+    var observer = new MutationObserver(function(mutationsList, observer) {
+        mutationsList.forEach(function(mutation) {
+            if (mutation.type === 'childList' || (mutation.type === 'attributes' && mutation.attributeName === 'class')) {
+                if (document.documentElement.classList.contains('notion-html')) {
+                    addStyleToElements();
+                }
+            }
+        });
     });
-  });
 
   observer.observe(document.documentElement, { attributes: true, childList: true, subtree: true });
 });
